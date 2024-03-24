e6286ec6280a148f87b7e3dff8c825431f13d238
deshruh <deshruhwork@gmail.com>
2024-03-24 17:39:10 +0500

    Created the CRUD for tables:
    - "Product", "ProductCategory" and "ProductTag"
    - alter fields "price" and "tags" from table "Product"
    - add new field - "old_price" - in table "Product"

--- products/models.py
+++ products/models.py
@@ -15,18 +15,18 @@ class ProductTag(models.Model):
     def __str__(self):
         return self.name
 
-
 class Product(models.Model):
     name = models.CharField(max_length=256)
     description = models.TextField()
-    price = models.DecimalField(max_digits=8, decimal_places=2)
+    old_price = models.DecimalField(max_digits=10, decimal_places=2, null = True, blank = True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
     quantity = models.PositiveIntegerField(default=0)
     images = models.ImageField(upload_to="product_images")
     category = models.ForeignKey(to=ProductCategory, on_delete=models.CASCADE)
-    tags = models.ForeignKey(to=ProductTag, on_delete=models.CASCADE)
+    tags = models.ForeignKey(to=ProductTag, on_delete=models.CASCADE, null = True, blank = True)
 
     def __str__(self):
-        return f"Продукт: {self.name} | Категория: {self.category}"
+        return f"Product: {self.name} | Category: {self.category}"
 
 
 class Cart(models.Model):
@@ -36,11 +36,11 @@ class Cart(models.Model):
     created_timestamp = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"Корзина для {self.user.username} | Товар: {self.product.name}"
+        return f"Cart by {self.user.username} | Product: {self.product.name}"
     
 class Wishlist(models.Model):
     user = models.ForeignKey(to=User, on_delete=models.CASCADE)
     product = models.ForeignKey(to=Product, on_delete=models.CASCADE)
 
     def __str__(self):
-        return f"Список желаемых товаров пользователя {self.user.username} | Товар: {self.product.name}"
+        return f"Wishlist by {self.user.username} | Product: {self.product.name}"
--- products/views.py
+++ products/views.py
@@ -1,14 +1,34 @@
 from django.shortcuts import render
+from products.models import Product, ProductCategory, ProductTag
 
 # Create your views here.
 def catalog(request):
-    return render(request, 'products/catalog.html')
+    context = {
+        "banner": True,
+        "banner_image": True,
+        "name_page": "Shop",
+        "products": Product.objects.all(),
+        "categories": ProductCategory.objects.all(),
+        "sale": ProductTag.objects.get(name = 'sale'),
+    }
+    
+    return render(request, 'products/catalog.html', context)
 
 def details(request):
     return render(request, 'products/product_details.html')
 
 def cart(request):
-    return render(request, 'products/cart.html')
+    context = {
+        "banner": True,
+        "banner_image": False,
+        "name_page": "Cart"
+    }
+    return render(request, 'products/cart.html', context)
 
 def wishlist(request):
-    return render(request, 'products/wishlist.html')
+    context = {
+        "banner": True,
+        "banner_image": False,
+        "name_page": "Wishlist"
+    }
+    return render(request, 'products/wishlist.html', context)
