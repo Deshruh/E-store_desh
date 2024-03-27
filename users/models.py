67318907f190c0f5bb247f868ed384d9e0ca931b
deshruh <deshruhwork@gmail.com>
2024-03-27 20:52:15 +0500

    Fix some issues

--- users/models.py
+++ users/models.py
@@ -5,10 +5,6 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
     image = models.ImageField(upload_to="users_images", null=True, blank=True)
     status = models.TextField(null=True, blank=True)
-<<<<<<< HEAD
     phone_number = models.PositiveBigIntegerField(null=True, blank=True)
-=======
-    phone_number = models.PositiveIntegerField(null=True, blank=True)
->>>>>>> e0ea8df6e5c80f6970e106a9500bf4df3f1aefdb
     # staff = models.CharField(max_length=50)
     # pay_card = ...
--- users/urls.py
+++ users/urls.py
@@ -3,11 +3,7 @@ from . import views
 
 urlpatterns = [
     path('login/', views.login, name = 'login'),
-<<<<<<< HEAD
     path('registration/', views.register, name = 'register'),
     path('profile/', views.profile, name = 'profile'),
     path('logout/', views.logout, name = 'logout')
-=======
-    path('registration/', views.register, name = 'register')
->>>>>>> e0ea8df6e5c80f6970e106a9500bf4df3f1aefdb
 ]
